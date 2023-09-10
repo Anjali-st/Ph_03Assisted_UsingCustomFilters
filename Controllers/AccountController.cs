f5c3128780d4641e8e8403fc7614c62f1a8048f8
Anjali-st <anjali90908080@gmail.com>
2023-09-11 00:08:50 +0530

    Using Custom Filters

---
+++ Controllers/AccountController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+using System.Collections.Generic;
+
+using System.Diagnostics;
+
+using System.Linq;
+
+using System.Threading.Tasks;
+
+using Microsoft.AspNetCore.Authorization;
+
+using Microsoft.AspNetCore.Mvc;
+
+using Microsoft.AspNetCore.Authentication;
+
+using Microsoft.AspNetCore.Authentication.Cookies;
+using System.Security.Claims;
+
+namespace Sec_04UsingCustomFilters.Controllers
+{
+    public class AccountController : Controller
+
+    {
+
+        public IActionResult Login()
+
+        {
+
+            return View();
+
+        }
+
+    }
+
+} 
+
